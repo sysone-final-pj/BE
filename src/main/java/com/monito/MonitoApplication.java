440d0547d55bebe3cc6ae46e6dda978db6404790
mint723 <mint723dev@gmail.com>
2025-10-13 11:33:07 +0900

    refactor: path 변경

--- src/main/java/com/monito/MonitoApplication.java
+++ src/main/java/com/monito/MonitoApplication.java
@@ -1,4 +1,4 @@
-package sys.monito;
+package com.monito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/monito/MonitoApplicationTests.java
+++ src/test/java/com/monito/MonitoApplicationTests.java
@@ -1,4 +1,4 @@
-package sys.monito;
+package com.monito;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
