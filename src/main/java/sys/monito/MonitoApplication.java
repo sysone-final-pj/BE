e6a13bce1e0d7170f8807f5b46b9e5dd8f8fa73b
mint723 <mint723dev@gmail.com>
2025-10-13 10:57:40 +0900

    Init Commit

---
+++ src/main/java/sys/monito/MonitoApplication.java
@@ -0,0 +1,13 @@
+package sys.monito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sys/monito/MonitoApplicationTests.java
@@ -0,0 +1,13 @@
+package sys.monito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
