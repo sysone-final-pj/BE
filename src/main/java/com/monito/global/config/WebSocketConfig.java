7528805bfa2c987fd6023b64b071edcfc9c6ec02
mint723 <neu990112@gmail.com>
2025-10-31 14:27:46 +0900

    feat: SCRUM-264 BE - AGENT 웹소켓 메시지 크기 변경

--- src/main/java/com/monito/global/config/WebSocketConfig.java
+++ src/main/java/com/monito/global/config/WebSocketConfig.java
@@ -4,10 +4,12 @@ import com.monito.domains.alert.websocket.handler.AlertWebSocketHandler;
 import com.monito.domains.agent.handler.AgentWebSocketHandler;
 import com.monito.domains.container.websocket.DashboardWebSocketHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
@@ -31,5 +33,18 @@ public class WebSocketConfig implements WebSocketConfigurer {
         // TODO : 대시보드 WebSocket 지표 항목 더 늘려야함
         registry.addHandler(dashboardWebSocketHandler, "/ws/dashboard")
                 .setAllowedOrigins("*");
+    }
+
+    /**
+     * 웹소켓 전송 설정
+     * - 메시지 크기 제한 설정 (64KB(기본) -> 1MB)
+     * - 버퍼 크기 설정
+     */
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(1024 * 1024);  // 1MB
+        container.setMaxBinaryMessageBufferSize(1024 * 1024);
+        return container;
     }
 }
