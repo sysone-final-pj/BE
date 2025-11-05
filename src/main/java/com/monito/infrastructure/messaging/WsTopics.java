64f68ec565eb1b89da4009ead04e627e513ab6ad
mint723 <neu990112@gmail.com>
2025-11-05 19:10:27 +0900

    feat: STOMP topic / sendMessage 구조

---
+++ src/main/java/com/monito/infrastructure/messaging/WsTopics.java
@@ -0,0 +1,8 @@
+package com.monito.infrastructure.messaging;
+
+public final class WsTopics {
+    public static final String CONTAINER_SUMMARY = "/topic/containers/summary";
+    public static String containerMetrics(Long containerId){
+        return "/topic/containers/" + containerId + "/metrics";
+    }
+}
