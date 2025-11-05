3ff5dade419526ff43bdc8c2bd839ef69e098f7e
mint723 <neu990112@gmail.com>
2025-11-05 14:18:26 +0900

    fix: agent 삭제 시 cache에 저장된 meta data 삭제

--- src/main/java/com/monito/domains/agent/service/AgentServiceImpl.java
+++ src/main/java/com/monito/domains/agent/service/AgentServiceImpl.java
@@ -12,6 +12,7 @@ import com.monito.domains.agent.repository.AgentRepository;
 import com.monito.domains.container.domain.Container;
 import com.monito.domains.container.domain.ContainerState;
 import com.monito.domains.container.repository.ContainerRepository;
+import com.monito.global.cache.AgentMetadataCache;
 import com.monito.global.exception.ExceptionMessage;
 import com.monito.global.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -28,6 +29,7 @@ import java.util.List;
 public class AgentServiceImpl implements AgentService {
     private final AgentRepository agentRepository;
     private final ContainerRepository containerRepository;
+    private final AgentMetadataCache agentMetadataCache;
 
     @Override
     @Transactional(readOnly = true)
@@ -105,6 +107,9 @@ public class AgentServiceImpl implements AgentService {
         // Soft delete all containers belonging to this agent
         containerRepository.findAllByAgent_Id(id)
                 .forEach(c -> c.markAsDeleted());
+
+        // agent 메타 데이터 삭제
+        agentMetadataCache.removeMetadata(agent.getAgentKey());
     }
 
     @Override
