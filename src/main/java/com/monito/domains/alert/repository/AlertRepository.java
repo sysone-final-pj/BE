2a48bdd6c61591c5b888a37dd1f5c4c51c4a33fe
dziimin <jmncrk04@gmail.com>
2025-11-17 17:06:59 +0900

    refactor : SCRUM-351 미사용 API 정리

--- src/main/java/com/monito/domains/alert/repository/AlertRepository.java
+++ src/main/java/com/monito/domains/alert/repository/AlertRepository.java
@@ -105,7 +105,7 @@ public interface AlertRepository extends JpaRepository<Alert, Long> {
             @Param("createdAtFromEmpty") boolean createdAtFromEmpty,
             @Param("createdAtTo") LocalDateTime createdAtTo,
             @Param("createdAtToEmpty") boolean createdAtToEmpty,
-            @Param("isRead") boolean isRead,
+            @Param("isRead") Boolean isRead,
             @Param("isReadEmpty") boolean isReadEmpty
     );
 }
