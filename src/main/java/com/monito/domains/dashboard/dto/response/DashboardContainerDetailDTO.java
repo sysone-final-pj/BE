60e26ae18d2b424ed9157dd258a60a2cfc0cf619
dziimin <jmncrk04@gmail.com>
2025-11-17 16:43:00 +0900

    refactor : SCRUM-351 미사용 API 정리

--- src/main/java/com/monito/domains/dashboard/dto/response/DashboardContainerDetailDTO.java
+++ src/main/java/com/monito/domains/dashboard/dto/response/DashboardContainerDetailDTO.java
@@ -12,7 +12,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
